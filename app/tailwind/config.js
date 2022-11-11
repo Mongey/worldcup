7fa346aacc2d09549ad68b8707333baf084c44b7
Conor Mongey <conor@mongey.net>
2022-11-11 01:46:01 +0000

    Upgrade pacakge

--- app/tailwind/config.js
+++ app/tailwind/config.js
@@ -3,14 +3,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const isProduction = EmberApp.env() === 'production';
 
 module.exports = {
-  purge: {
-    enabled: isProduction,
-    content: [
-      './app/index.html',
-      './app/templates/**/*.hbs',
-      './app/components/**/*.hbs',
-    ],
-  },
+  content: [
+    './app/index.html',
+    './app/templates/**/*.hbs',
+    './app/components/**/*.hbs',
+  ],
   presets: [],
   theme: {
     extend: {
