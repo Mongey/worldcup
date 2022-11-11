1682be9a1cf1b735dfbe0a00264ed75250661b52
Conor Mongey <conor@mongey.net>
2022-11-11 00:36:08 +0000

    fix

--- app/routes/worldcup.ts
+++ app/routes/worldcup.ts
@@ -12,7 +12,7 @@ export default class WorldCup extends Route {
   async model(params: { id: string }) {
     var players: Player[];
     var id = params.id;
-    if (id !== null) {
+    if (!id) {
       id = ENV.APP.default_group as string;
     }
     try {
